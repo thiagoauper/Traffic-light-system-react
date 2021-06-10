40e8932be0942ebcfeb74bb8e3cda0b77bb7fbd0
Thiago Perine <thiagoauper@gmail.com>
2021-06-10 11:37:28 +1000

    Including css class for Traffic Light Component.

--- src/components/TrafficLightComponent.tsx
+++ src/components/TrafficLightComponent.tsx
@@ -1,8 +1,9 @@
+import "./TrafficLightComponent.css";
 import LightComponent from "./LightComponent";
 
 function TrafficLightComponent(props: any) {
   return (
-    <div>
+    <div className="TrafficLight">
         <LightComponent color="Red" enabled={props.color === "Red"} />
         <LightComponent color="Yellow" enabled={props.color === "Yellow"} />
         <LightComponent color="Green" enabled={props.color === "Green"} />
