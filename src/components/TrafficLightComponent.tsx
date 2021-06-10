dfc26e5e3f17c2074b0d2734baeee964e64f26bc
Thiago Perine <thiagoauper@gmail.com>
2021-06-10 14:23:05 +1000

    Center aligning lights in the Traffic Lights component.

--- src/components/TrafficLightComponent.tsx
+++ src/components/TrafficLightComponent.tsx
@@ -4,9 +4,11 @@ import LightComponent from "./LightComponent";
 function TrafficLightComponent(props: any) {
   return (
     <div className="TrafficLight">
+      <div>
         <LightComponent color="Red" enabled={props.color === "Red"} />
         <LightComponent color="Yellow" enabled={props.color === "Yellow"} />
         <LightComponent color="Green" enabled={props.color === "Green"} />
+      </div>
     </div>
   );
 }
